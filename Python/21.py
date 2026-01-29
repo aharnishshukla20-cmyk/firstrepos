7bc4fdbb95a10203f5e1efbbf26a28bf33566274
aharnishshukla20-cmyk <aharnishshukla20@gmail.com>
2026-01-29 11:49:13 +0530

    Add python practice files and first html page

--- Python/21.py
+++ Python/21.py
@@ -1,38 +1,65 @@
-#ques 1
-# word = input("Enter a word: ")
-# reversed_word = ""
-
-# for i in range(len(word) - 1, -1, -1):
-#     reversed_word += word[i]
-
-# print("Reversed word:", reversed_word)
-
-#ques 2
-# n=int(input("enter the no of elements in list"))
-# a=[]
-# eve=[]
-# odd=[]
+# #ques 3
+# n=int(input("enter the number of elements"))
+# num=[]
 # for i in range(n):
-#     b=int(input("enter the elements"))
-#     a.append(b)
-# for i in a:
-#     if i>0:
-#         eve.append(i)
+#     b=int(input("enter the element"))
+#     num.append(b)
+# for i in num:
+#     if i<0:
+#         break
+# print(num)
+
+
+# numbers = [10, 20, 0, 5, 0, 25]
+# divisor = 5
+
+# for num in numbers:
+#     # Short-circuit logic prevents division by zero
+#     if num != 0 and divisor / num > 0:
+#         print(f"{divisor} / {num} = {divisor / num}")
 #     else:
-#         odd.append(i)
-# print(eve)
-# print(odd)
+#         print(f"Cannot divide by {num}")
 
-num = int(input("Enter a number: "))
-original = num
-rev = 0
+# while True:
+#     user_input = input("Enter something (type 'exit' to stop): ")
 
-while num > 0:
-    digit = num % 10
-    rev = rev * 10 + digit
-    num = num // 10
+#     if user_input.lower() == "exit":
+#         print("Program stopped.")
+#         break
 
-if rev == original:
-    print("Palindrome number")
-else:
-    print("Not a palindrome number")
+#     print(f"You entered: {user_input}")
+
+
+
+# while True:
+#     print("\n---- MENU ----")
+#     print("1. Addition")
+#     print("2. Subtraction")
+#     print("3. Multiplication")
+#     print("4. Exit")
+
+#     choice = input("Enter your choice (1-4): ")
+
+#     if choice == "4":
+#         print("Exiting program. Bye!")
+#         break
+
+#     if choice not in ("1", "2", "3"):
+#         print("Invalid choice! Try again.")
+#         continue
+
+#     a = float(input("Enter first number: "))
+#     b = float(input("Enter second number: "))
+
+#     if choice == "1":
+#         print("Result:", a + b)
+
+#     elif choice == "2":
+#         print("Result:", a - b)
+
+#     elif choice == "3":
+    #         print("Result:", a * b)
+
+
+
+
